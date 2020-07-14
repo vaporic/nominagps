5320e53cae625a3265826a10907a02cc3134135d
Hugo Espinosa <hugo@thethree.com.mx>
2020-07-13 21:32:47 -0500

    add

---
+++ platforms/browser/platform_www/cordova_plugins.js
@@ -0,0 +1,18 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "file": "plugins/cordova-plugin-native-spinner/www/SpinnerDialog.js",
+        "id": "cordova-plugin-native-spinner.SpinnerDialog",
+        "pluginId": "cordova-plugin-native-spinner",
+        "clobbers": [
+            "SpinnerDialog"
+        ]
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "cordova-plugin-native-spinner": "1.1.3"
+}
+// BOTTOM OF METADATA
+});
